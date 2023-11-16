36b9521d1ce00b93e89cd3dce291d73468d5318a
devkevin <rauppke@gmail.com>
2023-11-16 12:58:11 -0300

    Study JPA

---
+++ src/main/java/org/model/basico/Usuario.java
@@ -0,0 +1,48 @@
+package org.model.basico;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nome;
+    private String email;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Usuario(){
+
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/org/teste/basico/NovoUsuario.java
@@ -0,0 +1,24 @@
+package org.teste.basico;
+
+import org.model.basico.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class NovoUsuario {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("StudyJPA");
+        EntityManager em = emf.createEntityManager();
+
+        Usuario novoUsuario = new Usuario("teste","email@teste.com");
+
+
+        em.getTransaction().begin();
+        em.persist(novoUsuario);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
